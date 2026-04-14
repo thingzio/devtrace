e91f94515102336ad9e8e68a6c6c8246cbd95dee
Mark Chmarny <mark@chmarny.com>
2026-04-14 07:27:38 -0700

    Add migration for max_contributors column on shared tenant table

---
+++ pkg/data/postgres/sql/migrations/005_tenant_max_contributors.sql
@@ -0,0 +1,4 @@
+-- Add max_contributors column to tenant table.
+-- The column exists in DevTrace's CREATE TABLE definition (migration 001) but
+-- may be missing when the tenant table was originally created by DevPulse.
+ALTER TABLE tenant ADD COLUMN IF NOT EXISTS max_contributors INTEGER NOT NULL DEFAULT 50;
